2af1c900224d1c22adc8e3f450c4cfc4d64d7761
GerritPro <167561685+GerritPro@users.noreply.github.com>
2024-05-15 13:17:29 +0200

    Update scripts.js

--- CODE/scripts.js
+++ CODE/scripts.js
@@ -1,19 +1,116 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var colorButton = document.getElementById("colorButton1");
-// Füge einen Klick-Event-Listener zum Button hinzu
-    colorButton.addEventListener("click", function() {
-// Erstelle ein <input> Element vom Typ "color"
-    var colorInput = document.createElement("input");
-    colorInput.type = "color";
-// Füge einen Event-Listener zum Farbeingabe-Element hinzu
-    colorInput.addEventListener("input", function() {
-// Setze die Hintergrundfarbe der Seite entsprechend der ausgewählten Farbe
-    document.body.style.backgroundColor = colorInput.value;
-    });
-// Simuliere einen Klick auf das Farbeingabe-Element, um den Farbwähler zu öffnen
-    colorInput.click();
-    });
-});
+<script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
+  document.addEventListener('DOMContentLoaded', function () {
+    particlesJS('particles-js', {
+      particles: {
+        number: {
+          value: 100,
+          density: {
+            enable: true,
+            value_area: 800
+          }
+        },
+        color: {
+          value: "#ffffff"
+        },
+        shape: {
+          type: "circle",
+          stroke: {
+            width: 0,
+            color: "#000000"
+          },
+          polygon: {
+            nb_sides: 5
+          },
+          image: {
+            src: "img/github.svg",
+            width: 100,
+            height: 100
+          }
+        },
+        opacity: {
+          value: 0.5,
+          random: false,
+          anim: {
+            enable: false,
+            speed: 1,
+            opacity_min: 0.1,
+            sync: false
+          }
+        },
+        size: {
+          value: 3,
+          random: true,
+          anim: {
+            enable: false,
+            speed: 40,
+            size_min: 0.1,
+            sync: false
+          }
+        },
+        line_linked: {
+          enable: true,
+          distance: 150,
+          color: "#ffffff",
+          opacity: 0.4,
+          width: 1
+        },
+        move: {
+          enable: true,
+          speed: 6,
+          direction: "none",
+          random: false,
+          straight: false,
+          out_mode: "out",
+          bounce: false,
+          attract: {
+            enable: false,
+            rotateX: 600,
+            rotateY: 1200
+          }
+        }
+      },
+      interactivity: {
+        detect_on: "canvas",
+        events: {
+          onhover: {
+            enable: true,
+            mode: "repulse"
+          },
+          onclick: {
+            enable: true,
+            mode: "push"
+          },
+          resize: true
+        },
+        modes: {
+          grab: {
+            distance: 400,
+            line_linked: {
+              opacity: 1
+            }
+          },
+          bubble: {
+            distance: 400,
+            size: 40,
+            duration: 2,
+            opacity: 8,
+            speed: 3
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.4
+          },
+          push: {
+            particles_nb: 4
+          },
+          remove: {
+            particles_nb: 2
+          }
+        }
+      },
+      retina_detect: true
+    })
+  });
 
 document.addEventListener("DOMContentLoaded", function() {
     const myForm = document.getElementById("myForm");
