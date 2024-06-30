4e071e97b1238ac42b66a1404e0836f5308a387d
Jakob1097 <167630343+Jakob1097@users.noreply.github.com>
2024-06-30 10:50:20 +0200

    Update urls.py
    Datenschutzerklärung und Impressum bei urls.py verlinkt/hinzugefügt

--- sociall/urls.py
+++ sociall/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import signup_view, login_view, index, user_logout, profil
+from .views import signup_view, login_view, index, user_logout, profil, Datenschutz, Impressum
 
 urlpatterns = [
     path('', index, name='home'),
@@ -8,6 +8,8 @@ urlpatterns = [
     path('login/', login_view, name='login'),
     path('logout/', user_logout, name='logout'),
     path('SpeichernProfilbild/', profil, name='Profil'),
+    path('Datenschutzerklärung/', Datenschutz, name='Datenschutz'),
+    path('Impressum/', Impressum, name='Impressum'),
     path('save-residence/', views.save_residence, name='save_residence'),
     path('save-userpic/', views.save_userpic, name='save_userpic'),
 ]
